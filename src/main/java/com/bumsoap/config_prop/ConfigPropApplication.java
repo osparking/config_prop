945c9ebecba453c541e85228b464568c0c19c018
osparking <jbpark03@gmail.com>
2024-09-12 18:02:56 +0900

    created - configuration properties annotation test project

---
+++ src/main/java/com/bumsoap/config_prop/ConfigPropApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.config_prop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigPropApplication.class, args);
+	}
+
+}
