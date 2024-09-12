cc91133d2eae6843930724cf1d1473bad4024346
osparking <jbpark03@gmail.com>
2024-09-12 21:43:44 +0900

    remove a few Value anno by adding one class level anno

--- src/main/java/com/bumsoap/config_prop/pojo/GlobalProperties.java
+++ src/main/java/com/bumsoap/config_prop/pojo/GlobalProperties.java
@@ -1,16 +1,15 @@
 package com.bumsoap.config_prop.pojo;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:bumsoap.properties")
+@ConfigurationProperties
 public class GlobalProperties {
-    @Value(("${thread-pool}"))
     private int threadPool;
 
-    @Value(("${email}"))
     private String email;
 
     public int getThreadPool() {
