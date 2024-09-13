2bd9f691d29f5e30c702d5dbfabcb955d516ad59
osparking <jbpark03@gmail.com>
2024-09-13 14:21:04 +0900

    use JSR-303 validation annotations on global properties

--- src/main/java/com/bumsoap/config_prop/pojo/GlobalProperties.java
+++ src/main/java/com/bumsoap/config_prop/pojo/GlobalProperties.java
@@ -1,13 +1,22 @@
 package com.bumsoap.config_prop.pojo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties
+@Validated
 public class GlobalProperties {
+
+    @Max(5)
+    @Min(0)
     private int threadPool;
 
+    @NotEmpty
     private String email;
 
     public int getThreadPool() {
